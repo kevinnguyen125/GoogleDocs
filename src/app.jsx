8da5d5c27e36d44ce240643a096e42ab71fd170f
Howard Chong <hc757@cornell.edu>
2018-08-06 12:19:00 -0700

    @material-ui/core tested

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 export default class App extends React.Component {
   render() {
     return (<div>
+      <Button color="primary" variant="raised">Bold</Button>
       <h2>Welcome to React!</h2>
     </div>);
   }
