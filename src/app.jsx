214c198f31022c0331e905359a3a3fdf2e8ff6d0
Howard Chong <hc757@cornell.edu>
2018-08-06 14:45:01 -0700

    Moved styles, put red/blue box on the <Paper />

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Button, Grid, Paper } from '@material-ui/core';
 import { Editor, EditorState, RichUtils } from 'draft-js';
+import styles from './styles';
 
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.styles = styles;
     this.state = {
       editorState: EditorState.createEmpty(),
+      styling: styles.mainEditor,
     };
     this.onChange = editorState => this.setState({ editorState });
     this.setDomEditorRef = (ref) => {
@@ -32,12 +35,13 @@ export default class App extends React.Component {
         <Grid container justify="center" spacing={8}>
           <Grid item xs={8}>
             <Paper
+              style={this.state.styling}
               elevation={1}
-              style={{ padding: 10, height: 400 }}
               onClick={() => this.domEditor.focus()}
+              onFocus={() => this.setState({ styling: this.styles.mainEditorSelected })}
+              onBlur={() => this.setState({ styling: this.styles.mainEditor })}
             >
               <Editor
-                className="editor"
                 editorState={this.state.editorState}
                 onChange={this.onChange}
                 ref={this.setDomEditorRef}
