386128d895d154123ccc34cec19bd671830c50d2
Howard Chong <hc757@cornell.edu>
2018-08-06 12:43:34 -0700

    NOT WORKING: draft-js

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { Editor, EditorState, RichUtils } from 'draft-js';
+
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+    this.onChange = editorState => this.setState({ editorState });
+  }
+
+  onBoldClick(e) {
+    e.preventDefault();
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+
   render() {
     return (<div>
-      <Button color="primary" variant="raised">Bold</Button>
-      <h2>Welcome to React!</h2>
+      <Button color="primary" variant="raised" onMouseDown={e => this.onBoldClick(e)}>BOLD</Button>
+      <Editor editorState={this.state.editorState} onChange={this.onChange} />
     </div>);
   }
 }
